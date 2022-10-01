36431c17d4656cce84f34a9afbc080a086924b2a
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-01 21:21:01 +0500

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         int a=1, b=2;
-        double sum=(9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+        String sum=(9*Math.pow(a,2)-5*b+2+a-7)*((a+b-4*a*b)/2);
         System.out.println(sum);
     }
 }
