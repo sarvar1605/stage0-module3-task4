96d8ca62435f1b81a2ec92f730036e891ad4c34a
Sarvarbek <112868538+sarvar1605@users.noreply.github.com>
2022-10-01 21:09:09 +0500

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,9 +2,6 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char letter ='c';   
-        char letter2 ='h';
-        char letter3 ='a';
-        char letter4 ='r';
+       System.out.println((char)(99)+","+(char)(104)+","+(char)(97)+","+(char)(114));
     }
 }
